88cf6856e278240c4f56532ed74e316f0b60dc09
Mike Biancaniello <mikebianc@aol.com>
2014-10-16 18:29:26 -0400

    Modified DNS resolver to reply with nothing for a matching rule and proxy otherwise.

--- ofpc/dnspc.py
+++ ofpc/dnspc.py
@@ -4,6 +4,7 @@ import copy,sys
 from dnslib import DNSRecord,RR,QTYPE,RCODE,parse_time
 from dnslib.server import DNSServer,DNSHandler,BaseResolver,DNSLogger
 from dnslib.proxy import ProxyResolver,PassthroughDNSHandler,DNSHandler
+from pprint import pprint as pp
 
 #from . import dnsconf
 
@@ -23,11 +24,7 @@ class ParentalControls(object):
         self.TCP = True
 
     def start_dnsserver(self):
-        self.resolver = InterceptResolver(self.UP_IP,self.UP_PORT,'60s',
-            [
-                #Need to load from dnsconfig
-            ],[],[])
-        #handler = PassthroughDNSHandler
+        self.resolver = InterceptResolver(self,self.UP_IP,self.UP_PORT,'60s')
         handler = DNSHandler
         logger = DNSLogger("request,reply,truncated,error",False)
         self.udp_server = DNSServer(self.resolver,
@@ -35,13 +32,6 @@ class ParentalControls(object):
                             address=self.LOCAL_IP,
                             logger=logger,
                             handler=handler)
-        for rr in self.resolver.zone:
-            print "    | ",rr[2].toZone()
-        if self.resolver.nxdomain:
-            print "    NXDOMAIN:",", ".join(self.resolver.nxdomain)
-        if self.resolver.skip:
-            print "    Skipping:",", ".join(self.resolver.skip)
-        print
         self.udp_server.start_thread()
         if self.TCP:
             self.tcp_server = DNSServer(self.resolver,
@@ -61,20 +51,50 @@ class ParentalControls(object):
             self.tcp_server.stop()
 
     def add_rule(self,d=None,**kwargs):
+        print "add_rule({})".format(kwargs)
+        #pp(kwargs)
         self.rules.append(PCRule(**kwargs))
 
     def get_rules(self):
         return self.rules
 
-## note to self: change from object to dict later
+    def get_matching_rules(self,src_ip,dst_str):
+        rs = [ r for r in self.rules if r.is_match(src_ip,dst_str) ]
+        return rs
+
 class PCRule(object):
     def __init__(self,**kwargs):
-        self.src_ip = kwargs['src_ip']
-        self.dow = kwargs['dow']
-        self.time_start = kwargs['time_start']
-        self.time_end = kwargs['time_end']
-        self.dst_str = kwargs['dst_str']
-        self.action = kwargs['action']
+        self.src_ip = '*'
+        self.dst_str = '*'
+        self.dow = '*'
+        self.time_start = '*'
+        self.time_end = '*'
+        self.action = 'block'
+        for k in kwargs:
+            setattr(self,k,kwargs[k])
+    def is_match(self,src_ip=None,dst_str=None):
+        ## put in a for loop to verify each value is a match
+        #print "does {} from {} match {} from {}".format(dst_str,src_ip,self.dst_str,self.src_ip)
+        #print type(src_ip)
+        if src_ip != self.src_ip:
+            #print "    NO (IP)!"
+            return False
+        if not dst_str.matchGlob(self.dst_str):
+            #print "    NO (Domain)!"
+            return False
+        #print "    YES!"
+        return True
+    def _serialize(self,fields=None,skip=None):
+        retval = {}
+        if fields is None:
+            fields = self.__dict__.keys()
+        if skip is None:
+            skip = []
+        for f in fields:
+            if f in skip:
+                continue
+            retval[f] = getattr(self,f,None)
+        return retval
 
 class InterceptResolver(BaseResolver):
 
@@ -85,7 +105,7 @@ class InterceptResolver(BaseResolver):
         matching local records
     """
 
-    def __init__(self,address,port,ttl,intercept,skip,nxdomain):
+    def __init__(self,pcobj,address,port,ttl):
         """
             address/port    - upstream server
             ttl             - default ttl for intercept records
@@ -93,48 +113,58 @@ class InterceptResolver(BaseResolver):
             skip            - list of wildcard labels to skip 
             nxdomain        - list of wildcard labels to retudn NXDOMAIN
         """
+        self.PC = pcobj
         self.address = address
         self.port = port
         self.ttl = parse_time(ttl)
-        self.skip = skip
-        self.nxdomain = nxdomain
-        self.zone = []
-        for i in intercept:
-            if i == '-':
-                i = sys.stdin.read()
-            for rr in RR.fromZone(i,ttl=self.ttl):
-                self.zone.append((rr.rname,QTYPE[rr.rtype],rr))
 
     def add_zone(self,zone):
-        print "Adding Zone: {}".format(zone)
+        #print "Adding Zone: {}".format(zone)
         for rr in RR.fromZone(zone,ttl=self.ttl):
             self.zone.append((rr.rname,QTYPE[rr.rtype],rr))
-        print self.zone
+        #print self.zone
 
     def resolve(self,request,handler):
-        client_ip = handler.client_address
-        print "Request from IP: {}".format(client_ip)
+        client_ip = handler.client_address[0]
+        #print "Request from IP: {}".format(client_ip)
         reply = request.reply()
         qname = request.q.qname
         qtype = QTYPE[request.q.qtype]
+        #print "query: {}".format(qname)
+        #print "query: {}".format(qname.matchGlob("*.yahoo.com"))
         # Try to resolve locally unless on skip list
-        if not any([qname.matchGlob(s) for s in self.skip]):
-            for name,rtype,rr in self.zone:
-                if qname.matchGlob(name) and (qtype in (rtype,'ANY','CNAME')):
-                    a = copy.copy(rr)
-                    a.rname = qname
-                    reply.add_answer(a)
-        # Check for NXDOMAIN
-        if any([qname.matchGlob(s) for s in self.nxdomain]):
-            reply.header.rcode = getattr(RCODE,'NXDOMAIN')
-            return reply
-        # Otherwise proxy
-        if not reply.rr:
+        if any(self.PC.get_matching_rules(client_ip,qname)):
+            ## right now returning nothing
+            ## need to change to return based on rule match
+            ## (block, redirect, etc)
+            pass
+        else:
             if handler.protocol == 'udp':
                 proxy_r = request.send(self.address,self.port)
             else:
                 proxy_r = request.send(self.address,self.port,tcp=True)
             reply = DNSRecord.parse(proxy_r)
+        return reply
+        ###
+        # No longer Needed!!
+        #
+        #if not any([qname.matchGlob(s) for s in self.skip]):
+        #    for name,rtype,rr in self.zone:
+        #        if qname.matchGlob(name) and (qtype in (rtype,'ANY','CNAME')):
+        #            a = copy.copy(rr)
+        #            a.rname = qname
+        #            reply.add_answer(a)
+        ## Check for NXDOMAIN
+        #if any([qname.matchGlob(s) for s in self.nxdomain]):
+        #    reply.header.rcode = getattr(RCODE,'NXDOMAIN')
+        #    return reply
+        ## Otherwise proxy
+        #if not reply.rr:
+        #    if handler.protocol == 'udp':
+        #        proxy_r = request.send(self.address,self.port)
+        #    else:
+        #        proxy_r = request.send(self.address,self.port,tcp=True)
+        #    reply = DNSRecord.parse(proxy_r)
         return reply
 
 if __name__ == '__main__':
---
+++ test.sh
@@ -0,0 +1,8 @@
+curl -i -H "Content-Type: application/json" -X GET http://localhost:5000/start
+
+curl -i -H "Content-Type: application/json" -X POST -d '{"src_ip":"192.168.0.134","dow":"0,1,2,3,4","time_start":"18:00","time_end":"23:59","dst_str":"*.youtube.com","action":"block"}' http://localhost:5000/addfart
+curl -i -H "Content-Type: application/json" -X POST -d '{"src_ip":"127.0.0.1","dow":"0,1,2,3,4","time_start":"18:00","time_end":"23:59","dst_str":"*.youtube.com","action":"block"}' http://localhost:5000/addfart
+curl -i -H "Content-Type: application/json" -X GET http://localhost:5000/get/rules
+
+host www.youtube.com localhost
+
